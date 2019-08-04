e6cae359823b2b590357b84c916172f5d9cadd78
angelo foletto <angelofoletto@hotmail.com>
2019-08-04 11:12:17 -0300

    First commit
    Iniciando repositório.

---
+++ src/ex_01.java
@@ -0,0 +1,23 @@
+/*
+ *                     GNU GENERAL PUBLIC LICENSE
+ *                        Version 3, 29 June 2007
+ *
+ *  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ *  Everyone is permitted to copy and distribute verbatim copies
+ *  of this license document, but changing it is not allowed.
+ *
+ *                             Preamble
+ *
+ *   The GNU General Public License is a free, copyleft license for
+ * software and other kinds of works.
+ */
+
+/**
+ * @author neo
+ * @create 04/08/2019
+ */
+public class ex_01 {
+  public static void main(String[] args) {
+
+  }
+}
